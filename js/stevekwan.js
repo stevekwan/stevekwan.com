0de7e904052c33158cf9498e2d3dd9a1674611fe
Steve Kwan <mail@stevekwan.com>
2013-03-04 21:02:45 -0800

    Updates and some core JS improvements

--- js/stevekwan.js
+++ js/stevekwan.js
@@ -1,7 +1,15 @@
 var SteveKwan = SteveKwan || {};
 
-SteveKwan.sections = $("#header, #content, #footer");
+
+
+
+
+SteveKwan.sections       = $(".panel");
 SteveKwan.currentSection = $("#header");
+
+
+
+
 
 SteveKwan.scrollTo = function(element)
 {
@@ -14,6 +22,10 @@ SteveKwan.scrollTo = function(element)
   );
 };
 
+
+
+
+
 SteveKwan.handleScroll = function()
 {
   var currentY = $(window).scrollTop();
@@ -23,6 +35,8 @@ SteveKwan.handleScroll = function()
   (
     function(index, element)
     {
+      // If we are close to the edge of a panel, trigger a transition from
+      // the old one into the new one.
       var elementY = $(element).offset().top;
       var deltaY = currentY-elementY;
       if (deltaY < 0) deltaY *= -1;
@@ -31,37 +45,74 @@ SteveKwan.handleScroll = function()
       // We've scrolled into (or close to) a new section!
       if (SteveKwan.currentSection[0] != element)
       {
+        var oldSelection = SteveKwan.currentSection;
         var beforeNewSelection = SteveKwan.sections.slice(0,index);
         var afterNewSelection = SteveKwan.sections.slice(index);
         $(beforeNewSelection).children().fadeOut();
         $(afterNewSelection).children().fadeIn();
         SteveKwan.currentSection = $(element);
+
+        $(oldSelection).trigger('SteveKwan.exitPanel');
+        $(element).trigger('SteveKwan.enterPanel');
       }
     }
   );
 };
 
+
+
+
+
+SteveKwan.addTransitoryArrow = function(container, to, color)
+{
+  if (color === undefined) color = 'dark';
+
+  var transitoryArrow = $('<a href="'+to+'" class="transitory-arrow '+color+' hidden-phone ir">Scroll down</a>');
+
+  transitoryArrow.click
+  (
+    function(e)
+    {
+      e.preventDefault();
+      SteveKwan.scrollTo(to);
+    }
+  );
+
+  $(container).append(transitoryArrow);
+
+};
+
+
+
+
+
+SteveKwan.handleEnterPanel = function(e)
+{
+  console.log("Enter #" + $(e.target).attr('id'));
+};
+
+
+
+
+
+SteveKwan.handleExitPanel = function(e)
+{
+  console.log("Exit #" + $(e.target).attr('id'));
+};
+
+
+
+
+
 SteveKwan.handleReady = function()
 {
-  $('#header').find('.transitory-arrow').click
-  (
-    function(e)
-    {
-      e.preventDefault();
-      SteveKwan.scrollTo("#content");
-    }
-  );
+  // Set up transitory arrows that point to the next content region
+  SteveKwan.addTransitoryArrow('#header .fixed-region', '#expertise');
+  SteveKwan.addTransitoryArrow('#expertise', '#career', 'light');
+  SteveKwan.addTransitoryArrow('#career', '#portfolio');
+  SteveKwan.addTransitoryArrow('#portfolio', '#interests', 'light');
+  SteveKwan.addTransitoryArrow("#interests", "#footer");
 
-  $('#content').find('.transitory-arrow').click
-  (
-    function(e)
-    {
-      e.preventDefault();
-      SteveKwan.scrollTo("#footer");
-    }
-  );
-
-  //SteveKwan.sections.not(SteveKwan.currentSection).children().hide();
   SteveKwan.handleScroll();
 
   $(window).scroll
@@ -73,6 +124,12 @@ SteveKwan.handleReady = function()
     )
   );
 
+  $(SteveKwan.sections).bind('SteveKwan.enterPanel', SteveKwan.handleEnterPanel);
+  $(SteveKwan.sections).bind('SteveKwan.exitPanel', SteveKwan.handleExitPanel);
 };
+
+
+
+
 
 $(SteveKwan.handleReady);
